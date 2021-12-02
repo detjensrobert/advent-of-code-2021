6d21dd806d3fb1c835fd976ba1c0d57ac24bf9a5
Robert Detjens <github@detjens.dev>
2021-12-01 21:11:07 -0800

    day 2
    Signed-off-by: Robert Detjens <github@detjens.dev>

---
+++ day02/sol.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+input = ARGF.readlines
+
+# PART 1: Calculate the horizontal position and depth you would have after
+# following the planned course. What do you get if you multiply your final
+# horizontal position by your final depth?
+
+input.map! { |l| [l.split[0], l.split[1].to_i] }
+
+depth = 0
+h_pos = 0
+
+input.each do |dir, dist|
+  case dir
+  when 'forward'
+    h_pos += dist
+  when 'up'
+    depth -= dist
+  when 'down'
+    depth += dist
+  end
+end
+
+puts "pos * depth = #{depth * h_pos}"
+
+# PART 2: track a third value, aim that changes based on depth
+
+depth = 0
+h_pos = 0
+aim = 0
+
+input.each do |dir, dist|
+  case dir
+  when 'forward'
+    h_pos += dist
+    depth += dist * aim
+  when 'up'
+    aim -= dist
+  when 'down'
+    aim += dist
+  end
+end
+
+puts "pos * depth with aiming = #{depth * h_pos}"
