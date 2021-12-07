8e16ff4a798a61eeb0f6770fb26310f6df9fa982
Robert Detjens <github@detjens.dev>
2021-12-06 21:37:06 -0800

    Day 7, badly

---
+++ day07/sol.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+input = ARGF.read.split(',').map(&:to_i)
+
+# PART 1: Given a list of positions, find the position for which the least fuel
+# is spent to move to all listed positions. How much total fuel is spent?
+
+# sort all possible values by fuel cost
+starts = (input.min..input.max).map do |p|
+  [p, input.reduce(0) {|fuel, x| fuel + (p - x).abs }]
+end.sort_by { |x| x[1] }.to_h
+
+puts "Total fuel from best position #{starts.first.join(': ')}"
+
+# Part 2: Fuel cost is not linear and increases with distance. What is the total
+# cost from the new best starting position?
+
+# sort all possible values by fuel cost -- non-linear
+starts = (input.min..input.max).map do |p|
+  [p, input.reduce(0) {|fuel, x| fuel + (p - x).abs.downto(0).sum }]
+end.sort_by { |x| x[1] }.to_h
+
+puts "Total non-linear fuel from best position #{starts.first.join(': ')}"
