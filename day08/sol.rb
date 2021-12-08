6e927dd4839a4102ffc1b0d553e65fcb39e14e49
Robert Detjens <github@detjens.dev>
2021-12-08 00:03:39 -0800

    use hash instead of case
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- day08/sol.rb
+++ day08/sol.rb
@@ -22,33 +22,20 @@ puts "Number of 1/4/7/8s: #{count_1478}"
 # Use top/middle/bottom/topleft/... instead of a/b/c/d/... for clarity
 SSD = Struct.new(*%i[top mid bot tl bl tr br])
 
-# now that we have the full mapping, calculate the output digits
+# get digit from segment pattern and mapping
 def segment_to_digit(segments, m)
-  case segments.chars.sort
-  when [m.tr, m.br].sort
-    '1'
-  when [m.top, m.tr, m.mid, m.bl, m.bot].sort
-    '2'
-  when [m.top, m.tr, m.mid, m.br, m.bot].sort
-    '3'
-  when [m.tl, m.tr, m.mid, m.br].sort
-    '4'
-  when [m.top, m.tl, m.mid, m.br, m.bot].sort
-    '5'
-  when [m.top, m.tl, m.mid, +m.bl, m.br, m.bot].sort
-    '6'
-  when [m.top, m.tr, m.br].sort
-    '7'
-  when [m.top, m.tl, m.tr, m.mid, m.bl, m.br, m.bot].sort
-    '8'
-  when [m.top, m.tl, m.tr, m.mid, m.br, m.bot].sort
-    '9'
-  when [m.top, m.tl, m.tr, m.bl, m.br, m.bot].sort
-    '0'
-  else
-    print_mapping(m)
-    raise "unknown pattern: #{segments}"
-  end
+  {
+    [m.tr, m.br].sort => '1',
+    [m.top, m.tr, m.mid, m.bl, m.bot].sort => '2',
+    [m.top, m.tr, m.mid, m.br, m.bot].sort => '3',
+    [m.tl, m.tr, m.mid, m.br].sort => '4',
+    [m.top, m.tl, m.mid, m.br, m.bot].sort => '5',
+    [m.top, m.tl, m.mid, +m.bl, m.br, m.bot].sort => '6',
+    [m.top, m.tr, m.br].sort => '7',
+    [m.top, m.tl, m.tr, m.mid, m.bl, m.br, m.bot].sort => '8',
+    [m.top, m.tl, m.tr, m.mid, m.br, m.bot].sort => '9',
+    [m.top, m.tl, m.tr, m.bl, m.br, m.bot].sort => '0'
+  }[segments.chars.sort]
 end
 
 def print_mapping(m)
@@ -66,6 +53,7 @@ end
 output_values = input.map do |line, output|
   mapping = SSD.new
 
+  # find unique patterns (from part 1)
   one = line.find { |n| n.length == 2 }.chars
   four = line.find { |n| n.length == 4 }.chars
   seven = line.find { |n| n.length == 3 }.chars
@@ -73,21 +61,18 @@ output_values = input.map do |line, output|
 
   # segment in 7 and not in 1 is the top segment
   mapping.top = (seven - one)[0]
-  # puts "top seg: '#{mapping.top}'"
 
   # the 6-seg that has all segs in common with 4 is 9
   nine = line.find { |n| n.length == 6 && (four - n.chars).empty? }.chars
-  # puts "nine: #{nine.join}"
 
   # segment in 8 and not in 9 is bottom left
   mapping.bl = (eight - nine)[0]
 
-  # segment in 8 and not in 7 or in 4 or bl is bottom
+  # segment in 8 and not in 7 or in 4 or bot left is bottom
   mapping.bot = (eight - four - seven - [mapping.bl])[0]
 
-  # the 5-seg that has both bottom and botleft is 2
+  # the 5-seg that has both bottom and bot left is 2
   two = line.find { |n| n.length == 5 && n.index(mapping.bl) && n.index(mapping.bot) }.chars
-  # puts "two: #{two.join}"
 
   # segment in 9 but not in 2 or one is top left
   mapping.tl = (nine - two - one)[0]
@@ -101,12 +86,7 @@ output_values = input.map do |line, output|
   # four minus topleft, topright, botright is mid
   mapping.mid = (four - [mapping.tl, mapping.tr, mapping.br])[0]
 
-  digits = output.map { |s| segment_to_digit(s, mapping) }.join
-
-  # puts "output digits: #{digits}"
-  digits
+  output.map { |s| segment_to_digit(s, mapping) }.join
 end
 
-# puts output_values
-
-puts "sum of all outputs: #{output_values.map(&:to_i).sum}"
+puts "Sum of all outputs: #{output_values.map(&:to_i).sum}"
